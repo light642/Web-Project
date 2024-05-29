98447b01b6ab8fbef580b6b7dbee8813f5690bf1
liubo <2998739650@qq.com>
2024-05-29 22:00:57 +0800

    feat: 删除项目多余注释

--- forward/src/main.js
+++ forward/src/main.js
@@ -4,11 +4,7 @@ import router from '@/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './mock'
-//import axios from 'axios'
 
-
-
-//axios.defaults.baseURL = 'https://localhost:8080';
 createApp(App)
     .use(router)
     .use(ElementPlus)
