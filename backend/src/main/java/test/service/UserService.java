68307d221fe977f457ec6c504cf1e1bb18167129
newbie0118 <117242923+newbie0118@users.noreply.github.com>
2024-06-03 23:41:46 +0800

    Update UserService.java

--- backend/src/main/java/test/service/UserService.java
+++ backend/src/main/java/test/service/UserService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import test.entity.User;
 
 public interface UserService extends IService<User> {
+  List<UserService> getUserService(String username);
+  List<UserService> getRoomService(int id);
 }
