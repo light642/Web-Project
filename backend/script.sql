2574ea4ba83c967034362b3dcc1b6c70dd0fb180
newbie0118 <117242923+newbie0118@users.noreply.github.com>
2024-06-03 17:51:55 +0800

    Add files via upload

---
+++ backend/script.sql
@@ -0,0 +1,36 @@
+create table reservation
+(
+    id         int auto_increment
+        primary key,
+    room_id    int       not null,
+    username   char(128) not null,
+    start_time date      not null,
+    end_time   date      not null,
+    type       char(16)  not null
+);
+
+create table room
+(
+    id        int auto_increment
+        primary key,
+    room_type char(128) null,
+    location  char(128) null
+);
+
+create table user
+(
+    username char(128) not null
+        primary key,
+    password char(128) not null,
+    role     char(128) not null
+);
+
+create table user_message
+(
+    id       int auto_increment
+        primary key,
+    username char(120) not null,
+    message  char(255) not null
+);
+
+
